2a0fc572bf9a7009610b95f03ae20bca99232b8b
xxxzz25 <xxxzz925@gmail.com>
2018-01-03 00:57:19 +0800

    55555

---
+++ assets/js/numberCard.js
@@ -0,0 +1,62 @@
+function createCard() {
+    let find = document.getElementById('find');
+    let wait = document.getElementById('wait');
+    let findQty = '678';
+    let waitQty = '190';
+    console.log(findQty);
+    for (const iterator of findQty) {
+        createNumber(find, iterator);
+    }
+    for (const iterator of waitQty) {
+        createNumber(wait, iterator);
+    }
+
+    function createNumber(obj, qty) { //stage > content > (ul)icon > (li)stage-1-icon-$ > (span*2)stage-1-icon-top&bottom
+        let stage = document.createElement('div');
+        let content = document.createElement('div');
+        let icon = document.createElement('ul');
+        let time = .1;
+        Object.assign(stage, {
+            id: 'stage-1',
+            className: 'stage stage-state-default',
+        });
+        Object.assign(content, {
+            className: 'stage-content'
+        });
+        Object.assign(icon, {
+            className: 'stage-1-icon'
+        });
+        for (let i = 0; i <= qty; i++) {
+            let li = document.createElement('li');
+            let top = document.createElement('span');
+            let bottom = document.createElement('span');
+            Object.assign(li, {
+                className: 'stage-1-icon-' + i,
+            });
+            if (i == qty) {
+                Object.assign(top, {
+                    className: 'stage-1-icon-top'
+                });
+                top.style.webkitAnimationPlayState = "paused";
+                Object.assign(bottom, {
+                    className: 'stage-1-icon-bottom'
+                });
+            } else {
+                Object.assign(top, {
+                    className: 'stage-1-icon-top'
+                });
+                Object.assign(bottom, {
+                    className: 'stage-1-icon-bottom'
+                });
+            }
+            time += 0.2;
+            li.appendChild(top);
+            li.appendChild(bottom);
+            icon.appendChild(li);
+        }
+        content.appendChild(icon);
+        stage.appendChild(content);
+        obj.insertBefore(stage,obj.firstChild);
+    }
+}
+window.addEventListener('load', createCard);
