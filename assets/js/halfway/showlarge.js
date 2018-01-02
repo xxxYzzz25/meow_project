21d81c18786401cf716ec5724ef7eeab27de0302
xxxzz25 <xxxzz925@gmail.com>
2018-01-03 01:00:20 +0800

    55555

--- assets/js/halfway/showlarge.js
+++
@@ -1,7 +0,0 @@
-let pic = document.getElementsByClassName('picture');
-for (let i = 0, len = pic.length; i < len; i++) {
-    pic[i].addEventListener('click', function (e) {
-        let banner = document.getElementsByClassName('banner')[0];
-        banner.style.backgroundImage = "url(" + e.target.src + ")";
-    });
-}
--- assets/js/like.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function () {
-    function upDateLike() {//更新追蹤數量
-        let likeText = document.getElementById('like');
-        let likeList = sessionStorage.getItem('likeList').split('.');//分割字串為陣列 長度為追蹤數量
-        likeText.textContent = likeList.length;
-    }
-});
