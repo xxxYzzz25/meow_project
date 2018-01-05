95c4a0d6dc37f4836040c1ddd36dfd5a4fca9f59
ItsMeKevin1227 <c.k.louis1227@gmail.com>
2018-01-05 14:30:54 +0800

    js

--- js/signUp.js
+++ js/signUp.js
@@ -14,10 +14,12 @@ window.addEventListener('load', function () {
         let memIdLabel = userPhotoLabel.parentNode.parentNode.childNodes[7];
         let memPswLabel = userPhotoLabel.parentNode.parentNode.childNodes[13];
         let memBirthLabel = userPhotoLabel.parentNode.parentNode.childNodes[25];
+        let memAddLabel = userPhotoLabel.parentNode.parentNode.childNodes[31];
         memNameLabel.innerHTML = "會員暱稱<br><small>不得多於8個中/英文字元</small>";
         memIdLabel.innerHTML = "會員帳號<br><small>請輸入您的電子郵件</small>";
         memPswLabel.innerHTML = "會員密碼<br><small>請輸入6~10碼英數字</small>";
         memBirthLabel.innerHTML = "會員生日<br>";
+        memAddLabel.innerHTML = "通訊地址";
         console.log(userPhotoLabel.parentNode.parentNode.childNodes[25]);
         
     })
@@ -30,10 +32,12 @@ window.addEventListener('load', function () {
         let memIdLabel = userPhotoLabel.parentNode.parentNode.childNodes[7];
         let memPswLabel = userPhotoLabel.parentNode.parentNode.childNodes[13];
         let memBirthLabel = userPhotoLabel.parentNode.parentNode.childNodes[25];
+        let memAddLabel = userPhotoLabel.parentNode.parentNode.childNodes[31];
         memNameLabel.innerHTML = '中途之家名稱<br><small>不得多於8個中/英文字元</small>';
         memIdLabel.innerHTML = "中途帳號<br><small>請輸入您的電子郵件</small>";
         memPswLabel.innerHTML = "中途密碼<br><small>請輸入6~10碼英數字</small>";
         memBirthLabel.innerHTML = "中途生日<br>";
+        memAddLabel.innerHTML = "中途店址";
     })
 });
 $(document).ready(function () {
