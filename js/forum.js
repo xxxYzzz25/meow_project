e0525aeaca41f0e289470100d60ea53dbb6ee00f
utakatanotsuki <utakatanotsuki@gmail.com>
2018-01-08 20:30:08 +0800

    forum check

--- js/forum.js
+++ js/forum.js
@@ -1,23 +1,45 @@
-// document.getElementById("title-list-1").addEventListener("mouseover", hoverfunctoin1);
-// document.getElementById("title-list-2").addEventListener("mouseover", hoverfunctoin2);
-// document.getElementById("title-list-3").addEventListener("mouseover", hoverfunctoin3);
-document.getElementById("title-list-1").addEventListener("click", hoverfunctoin1);
-document.getElementById("title-list-2").addEventListener("click", hoverfunctoin2);
-document.getElementById("title-list-3").addEventListener("click", hoverfunctoin3);
+document.getElementById("f-1").addEventListener("click", clickfunctoin1);
+document.getElementById("f-2").addEventListener("click", clickfunctoin2);
+document.getElementById("f-3").addEventListener("click", clickfunctoin3);
+document.getElementById("f-4").addEventListener("click", clickfunctoin4);
+document.getElementById("b-1").addEventListener("click", clickfunctoin2);
+document.getElementById("b-2").addEventListener("click", clickfunctoin3);
+document.getElementById("b-3").addEventListener("click", clickfunctoin4);
 
 
-function hoverfunctoin1() {
-	document.getElementById("t-list-1").classList.add("dplay");
-	document.getElementById("t-list-2").classList.remove("dplay");
-	document.getElementById("t-list-3").classList.remove("dplay");
+function clickfunctoin1() {
+	document.getElementById("f-2").classList.remove("f-active");
+	document.getElementById("f-3").classList.remove("f-active");
+	document.getElementById("f-4").classList.remove("f-active");
+	document.getElementById("c-4").classList.remove("s-active");
+	document.getElementById("c-3").classList.remove("s-active");
+	document.getElementById("c-2").classList.remove("s-active");
+	document.getElementById("c-1").classList.add("s-active");
 }
-function hoverfunctoin2() {
-	document.getElementById("t-list-2").classList.add("dplay");
-	document.getElementById("t-list-1").classList.remove("dplay");
-	document.getElementById("t-list-3").classList.remove("dplay");
+function clickfunctoin2() {
+	document.getElementById("f-4").classList.remove("f-active");
+	document.getElementById("f-3").classList.remove("f-active");
+	document.getElementById("f-2").classList.add("f-active");
+	document.getElementById("c-4").classList.remove("s-active");
+	document.getElementById("c-3").classList.remove("s-active");
+	document.getElementById("c-1").classList.remove("s-active");
+	document.getElementById("c-2").classList.add("s-active");
 }
-function hoverfunctoin3() {
-	document.getElementById("t-list-3").classList.add("dplay");
-	document.getElementById("t-list-1").classList.remove("dplay");
-	document.getElementById("t-list-2").classList.remove("dplay");
+function clickfunctoin3() {
+	document.getElementById("f-4").classList.remove("f-active");
+	document.getElementById("f-2").classList.remove("f-active");
+	document.getElementById("f-3").classList.add("f-active");
+	document.getElementById("c-4").classList.remove("s-active");
+	document.getElementById("c-2").classList.remove("s-active");
+	document.getElementById("c-1").classList.remove("s-active");
+	document.getElementById("c-3").classList.add("s-active");
+}
+function clickfunctoin4() {
+	document.getElementById("f-2").classList.remove("f-active");
+	document.getElementById("f-3").classList.remove("f-active");
+	document.getElementById("f-4").classList.add("f-active");
+	document.getElementById("c-3").classList.remove("s-active");
+	document.getElementById("c-2").classList.remove("s-active");
+	document.getElementById("c-1").classList.remove("s-active");
+	document.getElementById("c-4").classList.add("s-active");
 }
