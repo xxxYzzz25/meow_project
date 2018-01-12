8229a1efedde6c718b416622d072457340439cf3
ItsMeKevin1227 <c.k.louis1227@gmail.com>
2018-01-12 18:30:13 +0800

    會員登入寫好了

--- php/signIn2Member.php
+++ php/signIn2Member.php
@@ -11,30 +11,33 @@
 
 <body>
     <?php
-        $memId = $_GET["memId"];
-        $memPsw = $_GET["memPsw"];
+        $memId = $_POST["memId"];
+        $memPsw = $_POST["memPsw"];
         try{
             require_once("connectBD103G2.php");
 
                 //準備好指令
-            $sql = "select * from member where memId=? and memPsw=?";
+            $sql = "select * from member where mem_Id=? and mem_Psw=?";
                 //執行該指令
             $statement = $pdo -> prepare( $sql );
             $statement -> bindValue(1, $memId);
-            $statement -> bindValue(2, $memPsw);
+            $statement -> bindValue(2, MD5($memPsw));
             $statement -> execute();
                 //檢查是否有此帳密
             if( $statement->rowCount() === 0){ //帳密錯誤
-                echo "帳密錯誤 , 請重新登入";
+                echo "<center>帳密錯誤 , 請重新登入</center>";
             }else{//帳密存在
                 $memRow = $statement -> fetch(PDO::FETCH_ASSOC);//取回資料錄
-                echo $memRow["memName"] , "您好~";//致歡迎詞
+                $_SESSION["MEM_ID"] = $memRow["MEM_ID"];
+		        $_SESSION["MEM_NAME"] = $memRow["MEM_NAME"];
+                echo "<center>", $memRow["MEM_NAME"] , "您好~</center>";//致歡迎詞
             }
         }catch( PDOException $e){
         echo "行號: ",$e->getLine(), "<br>";
         echo "訊息: ",$e->getMessage() , "<br>";
         }echo "<script type='text/javascript'>back()</script>";
         echo "<center>將在五秒後回到原網址</center>";
+
     ?>
     <script type='text/javascript'>
         setTimeout(function back(){
