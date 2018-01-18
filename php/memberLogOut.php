70df30c735f1a1b89ad31fe7c321dfc431448fbf
ItsMeKevin1227 <c.k.louis1227@gmail.com>
2018-01-18 22:31:48 +0800

    ++

--- php/memberLogOut.php
+++ php/memberLogOut.php
@@ -7,11 +7,28 @@
 <head>
 	<meta charset="utf-8">
 	<title>Examples</title>
+	<style>
+		#backNext{
+			cursor: pointer;
+			border-bottom: 1px solid #44f;
+			color: #44f;
+		}
+	</style>
 </head>
 <body>
 	<?php
 		session_destroy();
 	?>
-	<div>已登出，歡迎再回來</div>
+	<center>已登出，歡迎再回來</center><br>
+	<center><a src="#" id="backNext">點我回到上一頁</a></center>
+	<script>
+		window.addEventListener('load', ()=>{
+			let back = document.getElementById('backNext')
+			back.addEventListener('click', (e)=>{
+				e.preventDefault();
+				window.history.back()
+			})
+		})
+	</script>
 </body>
 </html>
