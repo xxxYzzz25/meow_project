30d933c84cde7f05ea10d1840562cfef36325461
utakatanotsuki <utakatanotsuki@gmail.com>
2018-01-19 17:26:13 +0800

    領養記錄查詢 done.

--- html/backAdobtRecord.php
+++ html/backAdobtRecord.php
@@ -52,30 +52,76 @@
                 <img src="../images/back/catAdoptRecord.jpg" alt="">
             </div>
             <div class="adoptInfomation">
-                <select name="sort" id="adobtSort">
+                <select name="sort" id="adobtOrder">
                     <option value="1">領養日期從新到舊</option>
                     <option value="2">領養日期從舊到新</option>
                 </select>
-                <table>
-                    <tr id="">
+                <table id="adobtList">
+                    <tr id="listTitle">
                         <th>喵小孩編號</th>
                         <th>領養者</th>
                         <th>領養時間</th>
                     </tr>
-                    <tr>
-                        <td>001</td>
-                        <td>abc@gmail.com</td>
-                        <td>2017/12/18</td>
-                    </tr>
                 </table>
-                
+
             </div>
         </div>
 
     </div>
-<script>
-    
-</script>
+    <script>
+        window.addEventListener('load', () => {
+
+            function getData() {
+
+                let res = JSON.parse(this.responseText);
+                let adobtList = document.getElementById('adobtList');
+                let fragment = document.createDocumentFragment();
+
+                for (const i of res) {
+                    let tr = document.createElement('tr');
+                    let catNoTd = document.createElement('td');
+                    let memIdTd = document.createElement('td');
+                    let timeTd = document.createElement('td');
+                    let catNo = document.createTextNode(i["喵編號"]);
+                    let memId = document.createTextNode(i["領養者"]);
+                    let time = document.createTextNode(i["領養時間"]);
+                    catNoTd.appendChild(catNo);
+                    memIdTd.appendChild(memId);
+                    timeTd.appendChild(time);
+                    tr.appendChild(catNoTd);
+                    tr.appendChild(memIdTd);
+                    tr.appendChild(timeTd);
+                    fragment.appendChild(tr);
+                }
+                adobtList.appendChild(fragment);
+            }
+
+            function changeOrder() {
+                let order = this.value;
+                let adobtList = document.getElementById('adobtList');
+                let listLen = adobtList.childElementCount;
+                
+                for (let i = listLen; i > 1; i--) {
+                    adobtList.lastChild.remove();
+                }
+                ajax(getData,`order=${order}`);
+            }
+
+            function ajax(cb, dataInfo) {
+
+                let xhr = new XMLHttpRequest();
+                xhr.open('post', '../php/backAdobtRecord_s.php');
+                xhr.onload = cb;
+                xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+                xhr.send(dataInfo);
+
+            }
+
+            let selector = document.getElementById('adobtOrder');
+            selector.addEventListener('change', changeOrder);
+            ajax(getData, 'order=1');
+        });
+    </script>
 </body>
 
 </html>
