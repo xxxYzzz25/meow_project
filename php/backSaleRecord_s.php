46962dc3167a9cda8d9d35a26133e0ab8ea07335
utakatanotsuki <utakatanotsuki@gmail.com>
2018-01-20 20:16:30 +0800

    銷售紀錄 & 領養紀錄 done.

---
+++ php/backSaleRecord_s.php
@@ -0,0 +1,50 @@
+
+<?php
+	try{
+        require_once('./connectBD103G2.php');
+
+        $order = $_POST["order"];
+        $qty = $_POST["qty"];
+
+        if(isset($_POST["pages"])){
+			$sql = "select count(*) count
+					from ORDERLIST o
+					join MEMBER m
+					on o.MEM_NO = m.MEM_NO";
+
+			$data = $pdo -> prepare($sql);
+			$data -> execute();
+			$dataRow = $data -> fetchObject();
+			echo $dataRow -> count;
+        }else{
+        
+			if($order == '1'){
+				$sql = "select o.ORDER_NO orderNo,m.MEM_ID memId,o.ORDER_TIME orderTime,o.ORDER_STATUS orderStatus  
+						from ORDERLIST o
+						join MEMBER m
+						on o.MEM_NO = m.MEM_NO
+						group by o.ORDER_NO
+						order by o.ORDER_TIME
+						limit $qty,20";
+			}else if($order == '2'){
+				$sql = "select o.ORDER_NO orderNo,m.MEM_ID memId,o.ORDER_TIME orderTime,o.ORDER_STATUS orderStatus  
+						from ORDERLIST o
+						join MEMBER m
+						on o.MEM_NO = m.MEM_NO
+						group by o.ORDER_NO
+						order by o.ORDER_TIME desc
+						limit $qty,20";
+			}
+			$data = $pdo -> query($sql);
+			$jsonData = array();
+			while ($dataRow = $data -> fetchObject()) {
+					array_push($jsonData,array('訂單編號' => $dataRow -> orderNo, '購買人帳號' => $dataRow -> memId, '購買時間' => $dataRow -> orderTime, '出貨狀態' => $dataRow -> orderStatus));
+		}
+		$jsonData = json_encode($jsonData);
+		echo $jsonData;
+        }
+    }catch(PDOException $e){
+		echo $e -> getMessage();
+		echo $e -> getLine();
+	}
+?>
